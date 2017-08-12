2e82f427625e22f193945df0bfc3b379386b47bf
MatthewFennell <m.fennell@live.co.uk>
2017-08-13 00:57:14 +0100

    Tidied up code (still falls down and moves left / right)

--- tetris.py
+++ tetris.py
@@ -2,8 +2,20 @@ from tkinter import *
 import time
 
 ################################################################################################################################
+
+grid_size = 25                                                                                                                      # how big the grid is (N x N)
+left_line_location = 100                                                                                                            # how big the middle area is (left side)
+right_line_location = 600                                                                                                           # how big the middle area is (right side)
+cell_size = int((right_line_location - left_line_location)/grid_size)                                                               # how big each cell is
+canvas_height = 500                                                                                                                 # how far down the screen
+canvas_width = 700                                                                                                                  # how far across the screen
+grid = [[0]*grid_size for i in range(0,cell_size-1)] + [[1]*cell_size for i in range(0, 3)]
+
+square_array = [[0]*grid_size for i in range(0, cell_size-1)]
+
+################################################################################################################################
 # Moves the block slowly down the screen
-counter = 0
+
 def move_blocks(label, shape):
     global box
     def count():
@@ -15,10 +27,11 @@ def move_blocks(label, shape):
             move_down()
         else:
             move_down()
-        label.config(text=str(counter))
         label.after(300,count)
     count()
+
 ################################################################################################################################
+# moves the square 1 down
 
 def move_down():
     global box
@@ -30,12 +43,18 @@ def move_down():
     else:
         box = canv.create_rectangle((left_line_location+right_line_location)/2, 0, (left_line_location+right_line_location)/2+grid_size,grid_size, fill = "green")
 
-def get_coords():                                                                                                              # returns column / row
+################################################################################################################################
+# return column / row co-ords of square
+
+def get_coords():                                                                                                             
     global box
     x_one, y_one, x_two, y_two = canv.coords(box)
     x_coord = (x_one - left_line_location)/grid_size
     y_coord = y_one / grid_size
     return [y_coord, x_coord]
+
+################################################################################################################################
+# moves the square 1 left
 
 def move_left():
     global box
@@ -45,6 +64,9 @@ def move_left():
     if canv.coords(box)[0] > left_line_location:
         canv.move(box,-grid_size,0)
 
+################################################################################################################################
+# moves the square 1 right
+
 def move_right():
     global box
     location = get_coords()
@@ -53,6 +75,8 @@ def move_right():
     if canv.coords(box)[0] < right_line_location - cell_size*2:
         canv.move(box,grid_size,0)
 
+################################################################################################################################
+# tracks keyboard input
 
 def callback(event):
     if event.keysym == 'a':
@@ -60,44 +84,46 @@ def callback(event):
     if event.keysym == 'd':
         move_right()
 
-grid_size = 25                                                                                                                      # how big the grid is (N x N)
-left_line_location = 100                                                                                                            # how big the middle area is (left side)
-right_line_location = 600                                                                                                           # how big the middle area is (right side)
-cell_size = int((right_line_location - left_line_location)/grid_size)                                                                    # how big each cell is
-canvas_height = 500                                                                                                                 # how far down the screen
-canvas_width = 700                                                                                                                  # how far across the screen
-grid = [[0]*grid_size for i in range(0,cell_size-1)] + [[1]*cell_size for i in range(0, 3)]
-
-square_array = [[0]*grid_size for i in range(0, cell_size-1)]
-
-
-
-root = Tk()
-canv = Canvas(root, width = canvas_width, height = canvas_height, highlightthickness=0)
-canv.pack(fill='both', expand=True)                                                                            # the screen layout
-##################################################################################################################################  
-top = canv.create_line(left_line_location, 0, right_line_location, 0, fill='green', tags=('top'))                                   # the top line 
-left = canv.create_line(left_line_location, 0, left_line_location, canvas_height, fill='green', tags=('left'))                      # the left line 
-
-right = canv.create_line(right_line_location, 0, right_line_location, canvas_height, fill='green', tags=('right'))                  # the right line
-
-bottom = canv.create_line(left_line_location, canvas_height, right_line_location, canvas_height, fill='red', tags=('bottom'))       # the bottom line
-
-left = canv.create_rectangle(0,0, left_line_location, canvas_height, fill = "#000080")                                              # the left square area
-
-right = canv.create_rectangle(right_line_location, 0, canvas_width, canvas_height, fill = "#000080")                                # the right square area
-
-middle = canv.create_rectangle(left_line_location, 0, right_line_location, canvas_height, fill = "#2f2f2f")                         # the center area 
 #####################################################################################################################################
+# creates the board structure
 
-global box
-box = canv.create_rectangle((left_line_location+right_line_location)/2, 0, (left_line_location+right_line_location)/2+grid_size,grid_size, fill = "yellow")
-label = Label(root, text="hi")
-move_blocks(label,box)
-root.title("counting seconds")
-canv.bind("<1>", lambda event: canv.focus_set())
-canv.bind("<Key>", callback)
-canv.pack()
+def create_board():
+    top = canv.create_line(left_line_location, 0, right_line_location, 0, fill='green', tags=('top'))                                   # the top line 
+    left = canv.create_line(left_line_location, 0, left_line_location, canvas_height, fill='green', tags=('left'))                      # the left line 
+    right = canv.create_line(right_line_location, 0, right_line_location, canvas_height, fill='green', tags=('right'))                  # the right line
+    bottom = canv.create_line(left_line_location, canvas_height, right_line_location, canvas_height, fill='red', tags=('bottom'))       # the bottom line
+    left = canv.create_rectangle(0,0, left_line_location, canvas_height, fill = "#000080")                                              # the left square area
+    right = canv.create_rectangle(right_line_location, 0, canvas_width, canvas_height, fill = "#000080")                                # the right square area
+    middle = canv.create_rectangle(left_line_location, 0, right_line_location, canvas_height, fill = "#2f2f2f")                         # the center area 
 
-root.mainloop()
+################################################################################################################################
+# creates a square shape at x,y
+
+def create_shape(x_coord, y_coord):
+    global box
+    x_location = x_coord * grid_size + left_line_location
+    y_location = y_coord * grid_size
+    box = canv.create_rectangle(x_location, y_location, x_location + grid_size, y_location + grid_size, fill = "red")
+    
+################################################################################################################################
+# starts the game
+
+def start_game():
+    global box
+    global canv
+    root = Tk()
+    canv = Canvas(root, width = canvas_width, height = canvas_height, highlightthickness=0)
+    canv.pack(fill='both', expand=True)                                                                            # the screen layout
+    create_board()
+    create_shape(10,0)
+    label = Label(root, text="Tetris")
+    move_blocks(label,box)
+    root.title("counting seconds")
+    canv.bind("<1>", lambda event: canv.focus_set())
+    canv.bind("<Key>", callback)
+    canv.pack()
+    root.mainloop()
+
+start_game()
+
 
